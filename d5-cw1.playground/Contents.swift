9050f1f56c8d86440ec0ddbd66f66e0ec667714a
Ziad Azab <100707085+Azab18@users.noreply.github.com>
2022-03-16 18:43:20 +0300

    done

--- d5-cw1.playground/Contents.swift
+++ d5-cw1.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let family = ["ziad", "mohamed", "eyad", "rara", "koki",]
+
+print(family.count)
+var evenNumbers = [2,4,6,8,10,12]
+evenNumbers.remove(at: 2)
+
+
+evenNumbers += [16,18,20,22]
+
+var money = [1,2,3]
+money.removeAll()
+var numbers = [1,2,3,4,5]
+for i in numbers {
+    print(i)
+}
